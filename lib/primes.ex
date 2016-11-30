51d99ea74bded590f9e5475d678aba5dfca350fd
Simone Vittori <hello@simonewebdesign.it>
2016-11-30 16:12:56 +0000

    Add Primes.first/1

--- lib/primes.ex
+++ lib/primes.ex
@@ -1,4 +1,7 @@
 defmodule Primes do
+  @moduledoc """
+  Utility functions for working with prime numbers
+  """
 
   @doc """
   Returns true if the number is prime, false otherwise.
@@ -20,11 +23,31 @@ defmodule Primes do
   iex> Primes.prime? -3
   false
   """
-  @spec prime?(number) :: boolean
-  def prime?(number) when number > 1 do
-    1..number
-    |> Stream.filter(&rem(number, &1) == 0)
+  @spec prime?(number()) :: boolean
+  def prime?(n) when n > 1 do
+    1..n
+    |> Stream.filter(&rem(n, &1) == 0)
     |> Enum.count == 2
   end
   def prime?(_), do: false
+
+  @doc """
+  Returns the first N prime numbers.
+
+  ## Examples
+
+  iex> Primes.first(3)
+  [2, 3, 5]
+  """
+  @spec first(pos_integer()) :: list
+  def first(0), do: []
+  def first(n) when n > 0 do
+    2
+    |> Stream.iterate(increment())
+    |> Stream.filter(&prime?(&1))
+    |> Stream.take(n)
+    |> Enum.to_list
+  end
+
+  defp increment, do: &(&1 + 1)
 end
